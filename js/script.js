61035d1bfa36c4e00ae0c30077775f5b3bb5b6c5
Christopher Myster <jp2107hh@my.hennepintech.edu>
2016-11-02 21:22:00 -0500

    weather app in class

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
-$.ready(function() {
-    
-   console.log("hello"); 
-    
-    
+$(function () {
+
+    var weatherKey = "";
+
+    $("#submit").on("click", function () {
+
+        if (weatherKey === "") {
+            weatherKey = $("#apikey").val();
+            if (weatherKey !== "") {
+                $("#api-div").hide();
+            }
+            console.log(weatherKey);
+        }
+        var zipCode = $("#zip").val();
+        $("#zip").val("");
+        console.log(zipCode);
+
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&units=imperial&APPID=" + weatherKey, function (data) {
+            console.log("yes!");
+            console.log(data.main.temp);
+            //city, description (in array)
+
+            $("#weather").append(data.main.temp);
+            //name weather[0]
+
+
+        });
+
+
+
+    });
+
+
+
+
+
+    console.log("hello");
+
+
 });
+
+// 31fe6ed8278fc00664a13d6ab4651af3
+
+//xhr requests are json requests
+
+//console.log(apikey);
