336b9fa34142eba38e530a39149f62c1459a9204
Hieu Pham <me4219fs@my.hennepintech.edu>
2016-11-02 21:21:36 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-$.ready(function() {
+$(function () {
     
-   console.log("hello"); 
-    
-    
+	var weatherKey = "";
+	
+    $("#submit").on("click", function () {
+		//Get the API key if present
+		if (weatherKey === "") {
+			weatherKey = $("#apikey").val();
+			if (weatherKey !== "") {
+				$("#api-div").hide();
+			}
+		}
+		
+		var zipCode = $("#zip").val();
+		$("#zip").val("");
+		
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&units=imperial&APPID=" + weatherKey, function (data) {
+			console.log("wooo");
+			console.log(data.main.temp);
+		});
+	});
 });
