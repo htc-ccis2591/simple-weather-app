d4770a80c0dd12b38699085a865dcf1cff134e77
cglaze001 <de0538qy@my.hennepintech.edu>
2016-11-02 21:44:27 -0500

    Working code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-$.ready(function() {
-    
-   console.log("hello"); 
-    
-    
-});
+(function () {
+    var weatherKey = "";
+    $("#submit").on("click", function () {
+        if (weatherKey === "") {
+            weatherKey = $("#apikey").val();
+            if (weatherKey !== "") {
+                $("#api-div").hide();
+            }
+        }
+        var zipCode = $("#zip").val();
+        $("#zip").val("");
+        console.log(zipCode);
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&units=imperial&APPID=" + weatherKey, function (data) {
+            console.log("WOO-HOO");
+            console.log(data.main.temp);
+        });
+    });
+
+}());
