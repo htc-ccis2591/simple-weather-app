e0123c91babe42b1ba33dd24da2880bcb720277a
kmbrandt94 <kmbrandt94@gmail.com>
2016-11-09 15:20:07 -0600

    Javascript Simple Weather App

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-$.ready(function() {
-    
-   console.log("hello"); 
-    
-    
+$(function () {
+
+    //c5d0500c3bda136b5b24ab20d6372d72
+    var weatherKey = "";
+    $("#submit").on("click", function () {
+        if (weatherKey === "") {
+            weatherKey = $("#apikey").val();
+            if (weatherKey !== "") {
+                //$("#apidiv").hide();
+            }
+        }
+        var zipCode = $("#zip").val();
+        $("#zip").val("")
+
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode +
+            ",us&units=imperial&APPID=" + weatherKey,
+            function (data) {
+                $("#weather").append("<br>It is " + data.main.temp + " degrees Fahrenheit in " + data.name + " with " + data.weather.description + ".");
+            });
+    });
 });
