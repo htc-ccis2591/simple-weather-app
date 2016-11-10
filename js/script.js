5091de5e116e3c42ac9585400fcdc7624cb77626
cglaze001 <de0538qy@my.hennepintech.edu>
2016-11-09 18:54:47 -0600

    fixed

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-(function () {
-    var weatherKey = "";
-    $("#submit").on("click", function () {
-        if (weatherKey === "") {
-            weatherKey = $("#apikey").val();
-            if (weatherKey !== "") {
+$(document).ready(function () {
+    var weatherkey = "";
+    $('#submit').click(function () {
+        //Get the API key if it is present
+        if (weatherkey === "") {
+            weatherkey = $('#apikey').val();
+            if (weatherkey !== "") {
                 $("#api-div").hide();
             }
         }
-        var zipCode = $("#zip").val();
-        $("#zip").val("");
-        console.log(zipCode);
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&units=imperial&APPID=" + weatherKey, function (data) {
-            console.log("WOO-HOO");
-            console.log(data.main.temp);
+        var zipCode = $('#zip').val();
+        $('#zip').val("");
+        //This JSON call gets the current name of the city by Zipcode, and the current weather in fahrenheit.
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&units=imperial&APPID=" + weatherkey, function (data) {
+            var myWeather = "The Local Weather Guru in " + data.name + " " + "calls for a" + " " + data.weather[0].description.toLowerCase() + " " + "and a temperature of" + " " + data.main.temp + " " + "degrees fahrenheit.";
+            $('section').next().html('<label>' + myWeather + '</label>');
+            console.log(myWeather);
         });
     });
-
-}());
+});
